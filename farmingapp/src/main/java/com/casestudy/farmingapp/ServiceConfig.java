99bf788199593d40a07d1bfc8a3e45586cee6ef7
LiamMcdear123456 <44975086+LiamMcdear123456@users.noreply.github.com>
2019-01-25 14:25:01 +0000

    dfsd

--- farmingapp/src/main/java/com/casestudy/farmingapp/ServiceConfig.java
+++ farmingapp/src/main/java/com/casestudy/farmingapp/ServiceConfig.java
@@ -10,9 +10,9 @@ public class ServiceConfig extends ResourceConfig{
 	public ServiceConfig() {
 		register(CORSFilter.class);
 		register(CustomerAccessAPI.class);
-		register(FarmerAccessAPI.class);
-		register(ProvidedProduceAccessAPI.class);
-		register(VegAccessAPI.class);
+//		register(FarmerAccessAPI.class);
+//		register(ProvidedProduceAccessAPI.class);
+//		register(VegAccessAPI.class);
 		
 	}
 
