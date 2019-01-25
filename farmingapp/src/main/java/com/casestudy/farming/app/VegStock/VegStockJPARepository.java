c05dcc9f09594c8a6ee4ac81490c1c6b5ae40c84
LiamMcdear123456 <44975086+LiamMcdear123456@users.noreply.github.com>
2019-01-25 15:29:33 +0000

    VegStock class

---
+++ farmingapp/src/main/java/com/casestudy/farming/app/VegStock/VegStockJPARepository.java
@@ -0,0 +1,9 @@
+package com.casestudy.farming.app.VegStock;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Component;
+
+@Component
+public interface VegStockJPARepository extends CrudRepository<VegStock, Integer> {
+
+}
