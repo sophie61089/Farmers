c749fa96ea3cdb7a54609fef67d3f5cfdc03606c
sophie61089 <44974945+sophie61089@users.noreply.github.com>
2019-01-25 14:38:16 +0000

    Farmer

--- farmingapp/src/main/java/com/casestudy/farmingapp/ServiceConfig.java
+++ farmingapp/src/main/java/com/casestudy/farmingapp/ServiceConfig.java
@@ -2,15 +2,17 @@ package com.casestudy.farmingapp;
 
 import org.glassfish.jersey.server.ResourceConfig;
 
+import com.casestudy.farmingapp.farmers.FarmerAccessAPI;
+
 
 public class ServiceConfig extends ResourceConfig{
 	
 	public ServiceConfig() {
 		register(CORSFilter.class);
-		register(CustomerAccessAPI.class);
+		//register(CustomerAccessAPI.class);
 		register(FarmerAccessAPI.class);
-		register(ProvidedProduceAccessAPI.class);
-		register(VegAccessAPI.class);
+		//register(ProvidedProduceAccessAPI.class);
+		//register(VegAccessAPI.class);
 	}
 
 }
