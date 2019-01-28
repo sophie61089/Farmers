dc37ba7fdb5a88742e438038599d69d2f469bdd1
LiamMcdear123456 <44975086+LiamMcdear123456@users.noreply.github.com>
2019-01-28 09:24:49 +0000

    twetrrw

--- farmingapp/src/main/java/com/casestudy/farmingapp/ServiceConfig.java
+++ farmingapp/src/main/java/com/casestudy/farmingapp/ServiceConfig.java
@@ -2,11 +2,8 @@ package com.casestudy.farmingapp;
 
 import org.glassfish.jersey.server.ResourceConfig;
 import com.casestudy.farmingapp.farmers.FarmerAccessAPI;
-<<<<<<< HEAD
 import com.casestudy.farming.app.VegStock.VegStockAccessAPI;
-=======
 import com.casestudy.farming.app.VegStock.VegStock;
->>>>>>> branch 'master' of https://github.com/sophie61089/Farmers.git
 import com.casestudy.farmingapp.Customer.CustomerAccessAPI;
 import com.casestudy.farmingapp.farmerorders.FarmerOrder;
 
@@ -16,11 +13,8 @@ public class ServiceConfig extends ResourceConfig{
 		register(CORSFilter.class);
 		register(CustomerAccessAPI.class);
 		register(FarmerAccessAPI.class);
-<<<<<<< HEAD
 		register(VegStockAccessAPI.class);
-=======
 		register(FarmerOrder.class);
 		register(VegStock.class);
->>>>>>> branch 'master' of https://github.com/sophie61089/Farmers.git
 	}
 }
