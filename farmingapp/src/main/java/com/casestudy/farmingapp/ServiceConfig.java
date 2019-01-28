c2caf5a991ccab5f1a611615bcb17be2203002b7
LiamMcdear123456 <44975086+LiamMcdear123456@users.noreply.github.com>
2019-01-28 09:23:22 +0000

    changes to vegstock and serviceconfig

--- farmingapp/src/main/java/com/casestudy/farmingapp/ServiceConfig.java
+++ farmingapp/src/main/java/com/casestudy/farmingapp/ServiceConfig.java
@@ -2,6 +2,7 @@ package com.casestudy.farmingapp;
 
 import org.glassfish.jersey.server.ResourceConfig;
 import com.casestudy.farmingapp.farmers.FarmerAccessAPI;
+import com.casestudy.farming.app.VegStock.VegStockAccessAPI;
 import com.casestudy.farmingapp.Customer.CustomerAccessAPI;
 
 public class ServiceConfig extends ResourceConfig{
@@ -10,5 +11,6 @@ public class ServiceConfig extends ResourceConfig{
 		register(CORSFilter.class);
 		register(CustomerAccessAPI.class);
 		register(FarmerAccessAPI.class);
+		register(VegStockAccessAPI.class);
 	}
 }
