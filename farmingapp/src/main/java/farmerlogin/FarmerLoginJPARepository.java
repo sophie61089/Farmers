52b011c1e1a48fab27d97acb13f80167517c71c7
LiamMcdear123456 <44975086+LiamMcdear123456@users.noreply.github.com>
2019-01-28 15:21:13 +0000

    added component to farmer login repository

--- farmingapp/src/main/java/farmerlogin/FarmerLoginJPARepository.java
+++ farmingapp/src/main/java/farmerlogin/FarmerLoginJPARepository.java
@@ -1,8 +1,9 @@
 package farmerlogin;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Component;
 
-
+@Component
 public interface FarmerLoginJPARepository extends CrudRepository<FarmerLogin, Integer> {
 
 }
