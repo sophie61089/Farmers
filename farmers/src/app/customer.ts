712ff682c76bd214a57cc42180262e6696bf1436
LiamMcdear123456 <44975086+LiamMcdear123456@users.noreply.github.com>
2019-01-29 11:27:23 +0000

    changes to customer services

---
+++ farmers/src/app/customer.ts
@@ -0,0 +1,10 @@
+export interface Customer {
+    customerId: number;
+    name: string;
+    email: string;
+    address: string;
+    cardNumber: number;
+    cardType: string;
+    cvc: number;
+    expiryDate: string;
+}
