ee5bb405fc532417dccddca97a58d51c127cd260
michaelwood1996 <44974781+michaelwood1996@users.noreply.github.com>
2019-01-29 09:01:04 +0000

    logo

---
+++ farmers/src/app/find-us/find-us.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-find-us',
+  templateUrl: './find-us.component.html',
+  styleUrls: ['./find-us.component.css']
+})
+export class FindUsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
