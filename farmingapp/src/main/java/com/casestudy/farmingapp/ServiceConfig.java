1b4fa838f25cf69563e5359a3bffcbb977c52e67
LiamMcdear123456 <44975086+LiamMcdear123456@users.noreply.github.com>
2019-01-30 09:24:07 +0000

    sfdsd

--- farmingapp/src/main/java/com/casestudy/farmingapp/ServiceConfig.java
+++ farmingapp/src/main/java/com/casestudy/farmingapp/ServiceConfig.java
@@ -18,11 +18,8 @@ public class ServiceConfig extends ResourceConfig{
 		register(CustomerAccessAPI.class);
 		register(FarmerAccessAPI.class);
 		register(VegStockAccessAPI.class);
-<<<<<<< HEAD
 		register(FarmerOrderAccessAPI.class);
-=======
 		register(FarmerOrderAccessAPI.class);
 
->>>>>>> branch 'master' of https://github.com/sophie61089/Farmers.git
 	}
 }
