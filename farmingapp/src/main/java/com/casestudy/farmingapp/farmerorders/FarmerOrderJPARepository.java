971ef244c05861a9bb9b3fb92ec4607b1c7a5296
ben61088 <44975057+ben61088@users.noreply.github.com>
2019-01-30 09:24:17 +0000

    Deleted farmer login class

--- farmingapp/src/main/java/com/casestudy/farmingapp/farmerorders/FarmerOrderJPARepository.java
+++ farmingapp/src/main/java/com/casestudy/farmingapp/farmerorders/FarmerOrderJPARepository.java
@@ -6,4 +6,6 @@ import org.springframework.stereotype.Component;
 @Component
 public interface FarmerOrderJPARepository extends CrudRepository<FarmerOrder, Integer>{
 
+	
+	
 }
--- farmingapp/src/main/java/com/casestudy/farmingapp/farmers/FarmerJPARepository.java
+++ farmingapp/src/main/java/com/casestudy/farmingapp/farmers/FarmerJPARepository.java
@@ -1,9 +1,16 @@
 package com.casestudy.farmingapp.farmers;
 
+import java.util.List;
+
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Component;
 
 @Component
 public interface FarmerJPARepository extends CrudRepository<Farmer, Integer>{
+	
+	@Query("select f from Farmer f where f.email =:email")
+	List<Farmer> findByEmail(@Param("email")String emailAddress);
 
 }
