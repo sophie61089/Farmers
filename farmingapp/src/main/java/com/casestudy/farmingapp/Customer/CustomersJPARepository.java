cb602fc19031751d716c7f4a87260af0cf21f5d7
ben61088 <44975057+ben61088@users.noreply.github.com>
2019-01-31 11:56:18 +0000

    Added server call for customers to login

--- farmingapp/src/main/java/com/casestudy/farmingapp/Customer/CustomersJPARepository.java
+++ farmingapp/src/main/java/com/casestudy/farmingapp/Customer/CustomersJPARepository.java
@@ -1,10 +1,19 @@
 package com.casestudy.farmingapp.Customer;
 
+import java.util.List;
+
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Component;
+
+import com.casestudy.farmingapp.farmers.Farmer;
 
 
 @Component
 public interface CustomersJPARepository extends CrudRepository<Customer, Integer> {
+	
+	@Query("select c from Customer c where c.email =:email")
+	List<Customer> findByEmail(@Param("email")String emailAddress);
 
 }
